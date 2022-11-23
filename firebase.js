e00d7c74d53d93a70221338698040276fb5fb798
adembasharon <adembasharon816@gmail.com>
2022-11-23 10:20:44 +0300

    modified files

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-// import {GoogleAuthProvider,getAuth} from "firebase/auth"
+// import {getStorage} from "firebase/storage"
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
 // // Your web app's Firebase configuration
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
+// export const firebaseConfig = {
 //   apiKey: "AIzaSyCTnOOSX0mIN-FewkTuEEgdle-bNQ7kbls",
 //   authDomain: "bidding-365405.firebaseapp.com",
 //   projectId: "bidding-365405",
@@ -21,3 +21,4 @@
 // const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
+// export const storage=getStorage(app)
